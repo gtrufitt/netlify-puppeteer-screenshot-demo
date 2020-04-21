c3cef2c1d52c0e93368c8332c754270c6893f7fb
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-04-21 16:49:31 +0100

    Update take-screenshot.js

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -25,10 +25,7 @@ exports.handler = async (event, context) => {
   
     return {
         statusCode: 200,
-        body: JSON.stringify({ 
-            message: `Complete screenshot of ${pageToScreenshot}!`, 
-            buffer: screenshot 
-        })
+        body: screenshot
     }
 
 }
