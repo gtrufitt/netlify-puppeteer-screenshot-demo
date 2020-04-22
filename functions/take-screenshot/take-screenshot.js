696f0b0ee9631eb098b42ed83a1d2c19a83cfb45
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2020-04-22 17:12:28 +0100

    Update take-screenshot.js

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -5,10 +5,31 @@ exports.handler = async (event, context) => {
     
     const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
     
+    
+    
     if (!pageToScreenshot) return {
         statusCode: 400,
         body: JSON.stringify({ message: 'Page URL not defined' })
     }
+    
+    const autoScroll = async page => {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      let distance = 200;
+      window.scrollTo(0, 0);
+      let timer = setInterval(() => {
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 300);
+    });
+  });
+};
     
       const browser = await chromium.puppeteer.launch({
         args: chromium.args,
@@ -19,6 +40,7 @@ exports.handler = async (event, context) => {
       const page = await browser.newPage();
       await page.setViewport({width: 1900, height: 3500});
       await page.goto(pageToScreenshot);
+      await autoScroll(page, "Page 2:");
       const screenshot = await page.screenshot({ encoding: 'base64' });
      
       await browser.close();
